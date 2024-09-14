a023a8df6c13e683e141e124a51440c81a95f1d4
Eduarda <164544759+eduardapastore@users.noreply.github.com>
2024-09-14 16:14:04 +0000

    adição/ aba Features

--- src/sections/AboutUs.tsx
+++ src/sections/AboutUs.tsx
@@ -3,7 +3,8 @@
 export const AboutUs = () => {
   return (
     <section className="py-50 relative z-10 -mt-30 bg-blue-100 shadow-lg p-8 mx-auto max-w-4xl rounded-xl items-center " style={{ marginTop: '-10rem' }}>
-          <h1 className="py-4 text-4xl font-bold text-center mb-4 text-blue-950">Quem somos?</h1>
+          <h1 className="text-center
+        section-title bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">Quem somos?</h1>
           <p className="text-lg text-center mb">
           No coração da inovação em saúde, nosso serviço de melhoria tecnológica para hospitais redefine a experiência do paciente e a eficiência dos profissionais de saúde em nossa cidade. Com uma plataforma inteligente e intuitiva, conectamos você aos serviços médicos de maneira mais ágil e eficaz.
           </p>
--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,7 +1,93 @@
+import Image from 'next/image'
+import img1 from "@/assets/img1.png"
+import img2 from "@/assets/img2.png"
+import img3 from "@/assets/img3.png"
+import img4 from "@/assets/img4.png"
+
 export const Features = () => {
     return (
-      <div className="">
-        <h1>Veja nossas Funcionalidades!</h1>
+      <div className="container py-5 mt-20">
+        <h1 className="section-title mb-10">Veja nossas Funcionalidades!</h1>
+    
+        <div className="container1 max-w-auto py-5 flex gap-20 justify-center items-center">
+          <div className="  ">
+            <h1 className=" text-blue-950 text-4xl font-bold">Agendamento Online</h1>
+            <p className=" text-blue-950">Marque suas consultas de forma rápida e prática, a qualquer hora e de qualquer lugar. Escolha o médico, o horário ideal e receba a confirmação instantânea, tudo online.</p>
+            <a href="">
+              <button  className="mt-5 bg-blue-900 text-white text-xl px-6 py-4 rounded-full font-medium inline-flex hover:scale-110 transform transition-transform duration-300">Agende já!</button>
+            </a>
+          </div>
+
+          <div className="w-full max-w-md mx-400 mt-8">
+              <Image 
+                src={ img1 } 
+                alt="" 
+                layout="responsive" 
+                className="rounded-lg"
+                objectFit='contain'
+              />
+          </div>
+        </div>
+
+        <div className="container2 max-w-auto py-5 flex gap-20 justify-center items-center">
+          <div className="w-full max-w-md mx-400 mt-8">
+                <Image 
+                  src={ img2 } 
+                  alt="" 
+                  layout="responsive" 
+                  className="rounded-lg"
+                  objectFit='contain'
+                />
+            </div>
+
+          <div className="  ">
+            <h1 className=" text-blue-950 text-4xl font-bold">Agendamento Online</h1>
+            <p className=" text-blue-950">Marque suas consultas de forma rápida e prática, a qualquer hora e de qualquer lugar. Escolha o médico, o horário ideal e receba a confirmação instantânea, tudo online.</p>
+            <a href="">
+              <button  className="mt-5 bg-blue-900 text-white text-xl px-6 py-4 rounded-full font-medium inline-flex hover:scale-110 transform transition-transform duration-300">Agende já!</button>
+            </a>
+          </div>
+        </div>
+
+        <div className="container1 max-w-auto py-5 flex gap-20 justify-center items-center">
+          <div className="  ">
+            <h1 className=" text-blue-950 text-4xl font-bold">Agendamento Online</h1>
+            <p className=" text-blue-950">Marque suas consultas de forma rápida e prática, a qualquer hora e de qualquer lugar. Escolha o médico, o horário ideal e receba a confirmação instantânea, tudo online.</p>
+            <a href="">
+              <button  className="mt-5 bg-blue-900 text-white text-xl px-6 py-4 rounded-full font-medium inline-flex hover:scale-110 transform transition-transform duration-300">Agende já!</button>
+            </a>
+          </div>
+
+          <div className="w-full max-w-md mx-400 mt-8">
+              <Image 
+                src={ img3 } 
+                alt="" 
+                layout="responsive" 
+                className="rounded-lg"
+                objectFit='contain'
+              />
+          </div>
+        </div>
+
+        <div className="container2 max-w-auto py-5 flex gap-20 justify-center items-center">
+          <div className="w-full max-w-md mx-400 mt-8">
+                <Image 
+                  src={ img4 } 
+                  alt="" 
+                  layout="responsive" 
+                  className="rounded-lg"
+                  objectFit='contain'
+                />
+            </div>
+
+          <div className="  ">
+            <h1 className=" text-blue-950 text-4xl font-bold">Agendamento Online</h1>
+            <p className=" text-blue-950">Marque suas consultas de forma rápida e prática, a qualquer hora e de qualquer lugar. Escolha o médico, o horário ideal e receba a confirmação instantânea, tudo online.</p>
+            <a href="">
+              <button  className="mt-5 bg-blue-900 text-white text-xl px-6 py-4 rounded-full font-medium inline-flex hover:scale-110 transform transition-transform duration-300">Agende já!</button>
+            </a>
+          </div>
+        </div>
       </div>
     );
   };
